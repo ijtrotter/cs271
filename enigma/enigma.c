ffbc2b036530b3e39fbcf3b86dee40fcfc6807b4
ijtrotter <vespitalian@gmail.com>
2026-08-29 19:51:23 -0700

    backward direction added

--- enigma/enigma.c
+++ enigma/enigma.c
@@ -12,6 +12,16 @@ char rapply(char letter, char *rotor) {
         return rotor[letter-LETINDEX];
 }
 
+/* Non-cool slow inversion */
+char rundo(char letter, char *rotor){
+        int i = 0;
+        for ( ; i < 26 ; i++){
+                /* search for the letter index, add the index to A, get letter :) */
+                if (rotor[i] == letter) return i + LETINDEX; 
+        }
+        return letter;
+}
+
 int main(int argc, char **argv) {
         printf("%c\n", rapply(argv[1][0], ROTORFAS));
         return 0;

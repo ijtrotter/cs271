e458ad10243911460376ca17da96331e3f3fedb3
ijtrotter <vespitalian@gmail.com>
2026-08-29 22:28:24 -0700

    rotating rotor (in theory)

--- enigma/enigma.c
+++ enigma/enigma.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define mod(n, m) (((n % m) + m) % m) /* shoutout big C, shoutout stackoverflow, shoutout learning */
+
 #define ROTORFAS "BDFHJLCPRTXVZNYEIWGAKMUSQO"
 #define ROTORMED "AJDKSIRUXBLHWTMCQGZNPYFVOE"
 #define ROTORSLO "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
@@ -20,6 +22,38 @@ char rundo(char letter, char *rotor){
                 if (rotor[i] == letter) return i + LETINDEX; 
         }
         return letter;
+}
+
+char rotors(char letter) {
+        letter = rapply(letter, ROTORFAS);
+        letter = rapply(letter, ROTORMED);
+        letter = rapply(letter, ROTORSLO);
+
+        letter = rapply(letter, ROTORREF);
+
+        letter = rundo(letter, ROTORSLO);
+        letter = rundo(letter, ROTORMED);
+        letter = rundo(letter, ROTORFAS);
+
+        return letter;
+}
+
+char shiftl(char letter, int n){
+        int index = letter - LETINDEX; // get letter pos
+        return mod(index + n, NUMCHARS) + LETINDEX; // shift letter according to pos 
+}
+
+char shifts(int counter, int n){
+        int sarray[7] = {
+                counter % NUMCHARS,
+                (counter / NUMCHARS) % NUMCHARS,
+                (counter / (NUMCHARS * NUMCHARS)) % NUMCHARS,
+                0, // reflector does not rotate :exploding_head:
+                (counter / (NUMCHARS * NUMCHARS)) % NUMCHARS,
+                (counter / NUMCHARS) % NUMCHARS,
+                counter % NUMCHARS,
+        };
+        return sarray[n];
 }
 
 int main(int argc, char **argv) {
